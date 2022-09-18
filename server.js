2513533988fc923fda5637a58f197794ac479c61
Stepanova Valeriya <shestakovalera200@mail.ru>
2022-09-18 16:20:07 +0300

    Настройка package.json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('dist'));
+app.use(express.static(`${__dirname}/dist/`));
 
 app.listen(port, () => {
   console.log(`Chat listening on port ${port}`);
