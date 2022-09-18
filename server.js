c67c2050eda7fe76a02ef7d15797b401438f289e
Stepanova Valeriya <shestakovalera200@mail.ru>
2022-09-18 14:02:29 +0300

    Настройки server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('dist'));
+app.use('/', express.static('dist'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Chat listening on port ${port}`);
 });
